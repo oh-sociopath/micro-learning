329fcab07ec3d4d81d5417303c0da5af6d43a8be
Gevorg_Martirosyan <gevorg.martirosyan.1998.08@gmail.com>
2025-05-19 13:31:51 +0400

    feature-ML-02 - added signup, login logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcryptjs';
+import { SignUpDto } from './dto/signup.dto';
+import { LoginDto } from './dto/login.dto';
+
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private prisma: PrismaService,
+        private jwtService: JwtService,
+    ) {}
+
+    async signUp(signUpDto: SignUpDto): Promise<{ token: string }> {
+        const { email, password, role } = signUpDto;
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await this.prisma.user.create({
+            data: {
+                email,
+                password: hashedPassword,
+                role,
+            },
+        });
+
+        const token = this.jwtService.sign({ id: user.id, role: user.role });
+        return { token };
+    }
+
+    async login(loginDto: LoginDto): Promise<{ token: string }> {
+        const { email, password } = loginDto;
+        const user = await this.prisma.user.findUnique({ where: { email } });
+
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            throw new Error('Invalid email or password');
+        }
+
+        const token = this.jwtService.sign({ id: user.id, role: user.role });
+        return { token };
+    }
+}
