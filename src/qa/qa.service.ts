3c9b8ed0fce6329216cce5b0fe0733f39f2622b6
Gevorg_Martirosyan <gevorg.martirosyan.1998.08@gmail.com>
2025-05-21 16:38:46 +0400

    feature-ML-05 - add service logic

--- src/qa/qa.service.ts
+++ src/qa/qa.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class QaService {}
+export class QaService {
+    constructor(private prisma: PrismaService) {}
+
+    async askQuestion(studentId: number, lessonId: number, text: string) {
+        return this.prisma.question.create({
+            data: { text, studentId, lessonId },
+        });
+    }
+
+    async answerQuestion(instructorId: number, questionId: number, text: string) {
+        return this.prisma.answer.create({
+            data: { text, instructorId, questionId },
+        });
+    }
+}
