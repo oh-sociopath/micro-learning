1c3821996551292ebea75798efa2693be119a43d
Gevorg_Martirosyan <gevorg.martirosyan.1998.08@gmail.com>
2025-05-21 17:00:17 +0400

    feature-ML-08 - register cache module

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CoursesService } from './courses.service';
 import { CoursesController } from './courses.controller';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
+  imports: [CacheModule.register({
+    ttl: 5000, // milliseconds
+    max: 100, // maximum number of items in cache
+  })],
   providers: [CoursesService],
   controllers: [CoursesController]
 })
