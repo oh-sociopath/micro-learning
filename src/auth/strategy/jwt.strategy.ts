e45cd1b91d1b8b294ff71ee3d9ed8f4e1b9fead2
Gevorg_Martirosyan <gevorg.martirosyan.1998.08@gmail.com>
2025-05-21 14:25:47 +0400

    fix-ML-03 - add logic to check id inside the token

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,11 +1,12 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-    constructor(config: ConfigService) {
+    constructor(config: ConfigService, private prisma: PrismaService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             secretOrKey: config.get('JWT_SECRET') || 'default-secret',
@@ -14,6 +15,14 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
 
     // @ts-ignore
     async validate(payload) {
+        const user = await this.prisma.user.findUnique({
+            where: { id: payload.sub },
+        });
+
+        if (!user) {
+            throw new UnauthorizedException('User not found');
+        }
+
         return { userId: payload.sub, role: payload.role };
     }
 }
